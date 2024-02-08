3521d2dce74b2333235704efc0819c12208909fa
Jonas Widmark <jonaswidmark@yahoo.se>
2024-02-08 17:56:57 +0100

    work

--- Assets/Scripts/Actions/BuildAction.cs
+++ Assets/Scripts/Actions/BuildAction.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 
 public class BuildAction : BaseAction
 {
+    private string currentVerb = "Build";
+    private string currentNoun = "Shelter";
     public override string GetActionName()
     {
-        return "Build";
+        return currentVerb + " " + currentNoun;;
     }
     public override void TakeAction(Action onActionComplete)
     {
         Debug.Log("Building!");
         ActionStart(onActionComplete);
         missionManager.InitializeMission();
+    }
+    public string GetCurrentMission()
+    {
+        return currentVerb + " " + currentNoun;
+    }
+    public void SetCurrenctVerb(string currentVerb)
+    {
+        this.currentVerb = currentVerb;
+    }
+    public void SetCurrenctNoun(string currentNoun)
+    {
+        this.currentNoun = currentNoun;
     }
 }
