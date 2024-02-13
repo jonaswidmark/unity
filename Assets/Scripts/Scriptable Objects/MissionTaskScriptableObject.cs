45705599932afa329456255f558c9f974943254c
Jonas Widmark <jonaswidmark@yahoo.se>
2024-02-13 09:14:22 +0100

    SO mission task

---
+++ Assets/Scripts/Scriptable Objects/MissionTaskScriptableObject.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+
+[CreateAssetMenu(fileName = "New MissionTask", menuName = "CustomObjects/MissionTask")]
+public class MissionTask : ScriptableObject
+{
+    [SerializeField] int id;
+    [SerializeField] String key;
+    [SerializeField] String title;
+    [SerializeField] Transform fromTransform;
+
+    [SerializeField] Transform toTransform;
+
+    public int Id
+    {
+        get { return id; }
+        private set { id = value; }
+    }
+    public string Key
+    {
+        get { return key; }
+        private set { key = value; }
+    }
+    public string Title
+    {
+        get { return title; }
+        private set { title = value; }
+    }
+    public Transform FromTransform
+    {
+        get { return fromTransform; }
+        private set { fromTransform = value; }
+    }
+    public Transform ToTransform
+    {
+        get { return toTransform; }
+        private set { toTransform = value; }
+    }
+    
+    
+}
