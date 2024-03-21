7ae1bc974fb54b795fb2e9e853b99f91c13c93ab
Jonas Widmark <jonaswidmark@yahoo.se>
2024-03-21 06:58:09 +0100

    working with normalized distance

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,13 +17,11 @@ public class Player : MonoBehaviour, IClickable
     private Rigidbody rb = null;
     public event EventHandler OnIsPlayerWalking;
     public event EventHandler OnIsPlayerIdle;
+    [SerializeField] private Animator animator;
     private bool isInMovement = false;
     private bool isMovingForward = false;
     private bool isGrounded = false;
-    private Vector3 upVector3 = new UnityEngine.Vector3(0,1,0);
-    private Vector3 emptyVector3 = new UnityEngine.Vector3(0,0,0);
-    private Vector3 forwardVector3 = new UnityEngine.Vector3(0,0,1);
-    private Vector3 moveVector = new UnityEngine.Vector3(0,0,0);
+    
     [SerializeField] private LayerMask floor;
     private Transform targetTransform;
     public float duration = 15.0f; // Antal sekunder det tar att nå målet
@@ -31,13 +29,17 @@ public class Player : MonoBehaviour, IClickable
     private Vector3 initialPosition;
     private float elapsedTime = 0f;
     private bool isMoving = false;
-    public float collisionRadius = 1.0f;
+    public float collisionRadius = 2.0f;
+    private float initialDistanceToTarget;
+    private Collider targetCollider;
+    private Collider playerCollider;
+    private Vector3 closestTargetPoint;
+    private Vector3 targetColliderVector;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-        
     }
     private void Start()
     {
@@ -48,6 +50,7 @@ public class Player : MonoBehaviour, IClickable
         missionManager = MissionManager.Instance;
         missionManager.OnGoToTransform += MissionManager_OnGoToTransform;
         initialPosition = transform.position;
+        playerCollider = transform.GetComponent<Collider>();
     }
     private void Update()
     {
@@ -65,11 +68,11 @@ public class Player : MonoBehaviour, IClickable
 
     Vector3 targetDirection = (targetTransform.position - transform.position).normalized;
 
-    float rotationSpeed = 100.0f;
+    float rotationSpeed = 230.0f;
     bool rotationComplete = RotateTowardsDirection(targetDirection, rotationSpeed);
     if (!rotationComplete) return;
-
-    if (IsTouchingTarget())
+    
+    if (IsTouchingTarget() )
     {
         Debug.Log("IsTouchingTarget");
         missionManager.EndCurrentMissiontaskCountdown();
@@ -77,66 +80,19 @@ public class Player : MonoBehaviour, IClickable
         OnIsPlayerIdle?.Invoke(this, EventArgs.Empty);
         return;
     }
-    float minSpeed = 0.0f;
-    float maxSpeed = 0.05f;
-    // Beräkna totala avståndet till målet
-    float totalDistance = Vector3.Distance(transform.position, targetTransform.position);
-    //Debug.Log(totalDistance/0.05f * Time.deltaTime);
-    // Beräkna hur stor del av avståndet som redan har tagits
-    float remainingDistance = totalDistance * (1.0f - Mathf.Clamp01(elapsedTime / duration));
-
-    // Beräkna den nya hastigheten baserat på avståndet till målet
-    float speed = Mathf.Lerp(minSpeed, maxSpeed, remainingDistance / totalDistance);
-    //Debug.Log(speed);
-    // Flytta spelaren med den nya hastigheten
-    float step = speed * Time.deltaTime;
-    transform.position = Vector3.MoveTowards(transform.position, targetTransform.position, 0.05f);
-
+    float moveTowardsSpeed = 0.05f;
+    transform.position = Vector3.MoveTowards(transform.position, targetTransform.position, moveTowardsSpeed);
+    float distanceToTarget = Vector3.Distance(playerCollider.ClosestPointOnBounds(transform.position), targetCollider.ClosestPointOnBounds(targetTransform.position)) - collisionRadius;
+    float normalizedDistanceToTarget = Mathf.Clamp01(distanceToTarget / initialDistanceToTarget);
+    animator.SetFloat("distanceToTarget", normalizedDistanceToTarget);    
+    
     elapsedTime += Time.deltaTime;
 }
 
-    private void FixedUpdateX()
-    {
-        if (!isMoving)
-            return;
-        Vector3 newInitialPosition = new Vector3(transform.position.x, 0.0f, transform.position.z);
-        transform.position = newInitialPosition;
-        Vector3 targetDirection = (targetTransform.position - transform.position).normalized;
-        
-        float rotationSpeed = 100.0f;
-        bool rotationComplete = RotateTowardsDirection(targetDirection, rotationSpeed);
-        if (!rotationComplete) return;
-        if (IsTouchingTarget())
-        {
-            isMoving = false;
-            OnIsPlayerIdle?.Invoke(this, EventArgs.Empty);
-        } 
-        elapsedTime += Time.deltaTime;
-        
-        float t = Mathf.Clamp01(elapsedTime / duration);
-        
-        Vector3 newTargetPosition = new Vector3(targetTransform.position.x, 0.0f, targetTransform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, newTargetPosition, 0.1f);
-        if(transform.position.x-newTargetPosition.x < -0.5f || transform.position.z -newTargetPosition.z < -0.5f)
-        {
-            //transform.position = Vector3.Lerp(initialPosition, newTargetPosition, t);
-            Debug.Log(transform.position-newTargetPosition); 
-        }
-        
-         // Beräkna den nya positionen för spelaren
-        //Vector3 newPosition = Vector3.Lerp(initialPosition, newTargetPosition, elapsedTime / duration);
-    
-        // Flytta spelaren
-        //GetComponent<Rigidbody>().MovePosition(newPosition);
-
-       
-        
-        
-    }
     private bool IsTouchingTarget()
     {
         // Utför en sfärisk kollisionskontroll runt spelarens position med angiven radie
-        Collider[] colliders = Physics.OverlapSphere(transform.position, collisionRadius);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, collisionRadius / 2f);
 
         // Loopa igenom alla collider som hittades
         foreach (Collider collider in colliders)
@@ -162,6 +118,18 @@ public class Player : MonoBehaviour, IClickable
         duration = e.missionTask.TimeToExecute;
         isMoving = true;
         OnIsPlayerWalking?.Invoke(this, EventArgs.Empty);
+        
+        targetCollider = targetTransform.GetComponent<Collider>();
+        if (targetCollider != null)
+        {
+            closestTargetPoint = targetCollider.ClosestPoint(transform.position);
+            
+            targetColliderVector = new Vector3(targetCollider.bounds.size.x, targetCollider.bounds.size.y, targetCollider.bounds.size.z);
+            initialDistanceToTarget = Vector3.Distance(playerCollider.ClosestPointOnBounds(transform.position), targetCollider.ClosestPointOnBounds(targetTransform.position));
+            
+            collisionRadius = Mathf.Max(targetCollider.bounds.size.x, targetCollider.bounds.size.y, targetCollider.bounds.size.z);
+            Debug.Log(collisionRadius);
+        }
     }
     private bool RotateTowardsDirection(Vector3 targetDirection, float rotateSpeed)
     {
